930a6d6e41c41339efe40a2cba24a3d47a869dd4
fidonacci <fahd.jamai@outlook.com>
2021-10-15 16:45:17 +0200

    commandline launch with arguments

--- parite.py
+++ parite.py
@@ -2,36 +2,62 @@
 # coding: utf-8
 
 import argparse
-import analysis.csv as c_an
-import analysis.xml as x_an
+import logging as lg
+
+import analysis.csv_analysis as c_an
+import analysis.xml_analysis as x_an
 
 
+lg.basicConfig(level=lg.DEBUG)
 
 def parse_arguments():
+    
     parser = argparse.ArgumentParser()
-    parser.add_argument("-e", "--extension", help="""Type of file to analyse. Is it a CSV or an XML?""")
-    parser.add_argument("-d", "--datafile", help="""CSV file containing pieces of information about the members of parliament""")
+
+    parser.add_argument("-d","--datafile",help="""CSV file containing pieces of
+        information about the members of parliament""")
+    parser.add_argument("-ex", "--extension", help="""Kind of file to analyse. Is it a CSV or an XML?""")
+    parser.add_argument("-p","--byparty", action='store_true', help="""displays
+        a graph for each political party""")
+    parser.add_argument("-i","--info", action='store_true', help="""information about
+        the file""")
+    parser.add_argument("-n","--displaynames", action='store_true', help="""displays
+        the names of all the mps""")
+    parser.add_argument("-s","--searchname", help="""search for a mp name""")
+    parser.add_argument("-I","--index", help="""displays information about the Ith mp""")
+    parser.add_argument("-g","--groupfirst", help="""displays a graph groupping all the 'g'
+        biggest political parties""")
+    
     return parser.parse_args()
  
 
-if __name__ == "__main__":
-    args = parse_arguments()
-    datafile = args.datafile
 
-
+def main(): 
     
-    print(args)
 
-    if args.extension == 'xml':
-        try:
+    arguments = parse_arguments()
+   
+    
+
+    try:
+        datafile = arguments.datafile
+        if datafile == None:
+            raise Warning('You must indicate a datafile!')
+    except Warning as e:
+        lg.warning(e)
+    else:
+        # import pdb; pdb.set_trace()
+        if arguments.extension == 'xml':
             x_an.launch_analysis(datafile)
-        except:
-            print("Problème de fichier man! : " )
-    elif args.extension == 'csv':
-        try:
-            c_an.launch_analysis(datafile)
-        except:
-            print("Problème de fichier man! : ")
-        
-    
+        elif arguments.extension == 'csv':
+            c_an.launch_analysis(datafile, arguments.byparty, arguments.info, arguments.displaynames,
+                           arguments.searchname, arguments.index, arguments.groupfirst)
+            
+
+    finally:
+        lg.info('#################### Analysis is over ######################')
+
+if __name__ == '__main__':
+    main()
+
 
